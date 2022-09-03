93ec414bc281f33fe92b878f1ebad17ff2d5bc56
Furanshisuko17 <darkfran555@gmail.com>
2022-09-02 23:31:49 -0500

    remover comentarios innecesarios en .java

--- src/main/java/utp/trabajofinalweb/JAXRSConfiguration.java
+++ src/main/java/utp/trabajofinalweb/JAXRSConfiguration.java
@@ -3,10 +3,6 @@ package utp.trabajofinalweb;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-/**
- * Configures JAX-RS for the application.
- * @author Juneau
- */
 @ApplicationPath("resources")
 public class JAXRSConfiguration extends Application {
     
--- src/main/java/utp/trabajofinalweb/resources/JavaEE8Resource.java
+++ src/main/java/utp/trabajofinalweb/resources/JavaEE8Resource.java
@@ -4,10 +4,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-/**
- *
- * @author 
- */
 @Path("javaee8")
 public class JavaEE8Resource {
     
