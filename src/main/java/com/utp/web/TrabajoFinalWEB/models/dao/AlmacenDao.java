184c7fa121b1ca1957dde63c6d30796758d0c75d
6syan9 <melgar10barca@gmail.com>
2022-10-17 00:01:40 -0500

    Borrado de las entidades excepto persona

--- src/main/java/com/utp/web/TrabajoFinalWEB/models/dao/AlmacenDao.java
+++
@@ -1,9 +0,0 @@
-package com.utp.web.TrabajoFinalWEB.models.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.utp.web.TrabajoFinalWEB.models.entity.Almacen;
-
-public interface AlmacenDao extends JpaRepository<Almacen, Long> {
-	
-}
--- src/main/java/com/utp/web/TrabajoFinalWEB/models/dao/MarcaDao.java
+++
@@ -1,8 +0,0 @@
-package com.utp.web.TrabajoFinalWEB.models.dao;
-
-import com.utp.web.TrabajoFinalWEB.models.entity.Marca;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MarcaDao extends JpaRepository<Marca, Long> {
-	
-}
