db096781b4e719d6b53afee7666b739e52843fd1
Furanshisuko17 <darkfran555@gmail.com>
2022-10-18 17:09:25 -0500

    Update EmpleadoDao.java

--- src/main/java/com/utp/web/TrabajoFinalWEB/models/dao/EmpleadoDao.java
+++ src/main/java/com/utp/web/TrabajoFinalWEB/models/dao/EmpleadoDao.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmpleadoDao extends JpaRepository<Empleado, Integer> {
+public interface EmpleadoDao extends JpaRepository<Empleado, Long> {
 	
 	List<Empleado> findByDni(String dni);
 }
