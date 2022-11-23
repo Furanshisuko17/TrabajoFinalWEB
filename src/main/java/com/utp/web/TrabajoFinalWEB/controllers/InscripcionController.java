94698503cbb444f5ebdcaed7a2f35a213e508398
Furanshisuko17 <darkfran555@gmail.com>
2022-11-22 21:22:30 -0500

    Imports sin usar removidos

--- src/main/java/com/utp/web/TrabajoFinalWEB/controllers/InscripcionController.java
+++ src/main/java/com/utp/web/TrabajoFinalWEB/controllers/InscripcionController.java
@@ -1,28 +1,17 @@
 package com.utp.web.TrabajoFinalWEB.controllers;
 
-import java.sql.Timestamp;
-import java.time.Instant;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.utp.web.TrabajoFinalWEB.models.dao.InscripcionDao;
 import com.utp.web.TrabajoFinalWEB.models.dao.SedeDao;
-import com.utp.web.TrabajoFinalWEB.models.entity.Cliente;
 import com.utp.web.TrabajoFinalWEB.models.entity.Inscripcion;
-import com.utp.web.TrabajoFinalWEB.models.entity.Sede;
 import com.utp.web.TrabajoFinalWEB.services.implementation.InscripcionServiceImpl;
 
 @Controller
