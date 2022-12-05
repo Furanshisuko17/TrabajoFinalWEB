db9af3e92788f61b993fdceb0506f02bccc15ffe
6syan9 <melgar10barca@gmail.com>
2022-12-04 21:34:56 -0500

    Arreglo de header and footer y vista de usuario
    -Las imagenes del header y footer no se mostraban, ya se soluciono.
    -Se implemento el mostrar la lista de registros del usuario que este iniciando seccion.

--- src/main/java/com/utp/web/TrabajoFinalWEB/controllers/UsuarioController.java
+++ src/main/java/com/utp/web/TrabajoFinalWEB/controllers/UsuarioController.java
@@ -1,15 +1,39 @@
 package com.utp.web.TrabajoFinalWEB.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.utp.web.TrabajoFinalWEB.services.RegistroService;
+
+import lombok.var;
+
 @Controller
 public class UsuarioController {
+
+	@Autowired
+	private RegistroService registroService;
 
 	@GetMapping("/usuario")
 	public String mainUsuario(Model model) {
 		
+		Authentication authentication= SecurityContextHolder.getContext().getAuthentication();
+
+		String dni= "";
+
+		Object principal= authentication.getPrincipal();
+		if(principal instanceof UserDetails){
+			dni= ((UserDetails) principal).getUsername();
+		}else{
+			dni= principal.toString();
+		}
+
+		var registros= registroService.listarporDni(dni);
+		model.addAttribute("registros", registros);
 		
 		return "usuario";
 	}
